66755543c54bca1a0d295bf13b71d84d0e5177d0
robo-github <anandtp11@gmail.com>
2025-07-02 22:32:28 +0530

    Add .gitignore and initial project files

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+admin.site.register(UserProfile)
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(Appointment)
+admin.site.register(MedicalRecord)
+admin.site.register(Billing)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,76 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+# USER
+
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('doctor', 'Doctor'),
+        ('receptionist', 'Receptionist'),
+        ('patient', 'Patient'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+# PATIENT
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    contact = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+# DOCTOR
+
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    specialization = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+# APPOINTMENT
+
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.CharField(max_length=20, default='Scheduled')
+
+    def __str__(self):
+        return f"{self.date} - {self.patient}"
+
+# MEDICAL RECORD
+
+
+class MedicalRecord(models.Model):
+    appointment = models.OneToOneField(Appointment, on_delete=models.CASCADE)
+    diagnosis = models.TextField()
+    prescription = models.TextField()
+
+    def __str__(self):
+        return f"Record for {self.appointment.patient}"
+
+# BILLING
+
+
+class Billing(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    service = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Bill - {self.patient} - ₹{self.amount}"
